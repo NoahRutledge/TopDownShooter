6fcc4f643609e7df1953bf493b2971a8c347f809
NoahRutledge <noah.rutledge@gmail.com>
2018-06-26 22:37:37 -0500

    Updating Unity

--- Assets/Scripts/Enemy/chasePlayer.cs
+++ Assets/Scripts/Enemy/chasePlayer.cs
@@ -7,6 +7,7 @@ public class chasePlayer : MonoBehaviour {
     public Transform target;
     private Transform enemy;
     public float moveSpeed = 3.0f;
+    public float stopDistance = 1.5f;
 
 	// Use this for initialization
 	void Start () {
@@ -19,7 +20,7 @@ public class chasePlayer : MonoBehaviour {
         float dz = target.position.z - enemy.position.z;
 
         int x, z;
-        if(dx < 0.25 && dx > -0.25)
+        if(dx < stopDistance && dx > -stopDistance)
         {
             x = 0;
         }
@@ -27,7 +28,7 @@ public class chasePlayer : MonoBehaviour {
         {
             x = dx > 0 ? 1 : -1;
         }
-        if(dz < 0.25 && dz > -0.25)
+        if(dz < stopDistance && dz > -stopDistance)
         {
             z = 0;
         }
